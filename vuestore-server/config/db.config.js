5653f6f394f9a74471bc50caa90919b6f447a1c7
Hary Capri <76942747+harycp@users.noreply.github.com>
2024-06-04 10:59:15 +0700

    change url db.config.js

--- vuestore-server/config/db.config.js
+++ vuestore-server/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb+srv://mevn-store:6R6M1bktghFc4xqv@mevn-store.gfsrlin.mongodb.net/?retryWrites=true&w=majority&appName=mevn-store",
+  url: "mongodb+srv://mevn-store:6R6M1bktghFc4xqv@mevn-store.gfsrlin.mongodb.net/collection_vuestore?retryWrites=true&w=majority&appName=mevn-store",
 };
